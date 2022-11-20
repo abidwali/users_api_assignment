18c6ee40c92f726886b475f18f5ad5e211308e70
Abid Wali <abidwali14@gmail.com>
2022-11-20 12:36:06 +0530

    Internet permission added for production mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'screens/home_screen.dart';
 
-
-
 void main() {
   runApp(const MyApp());
 }
@@ -15,10 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Zekadamy',
       theme: ThemeData(),
       home: const HomeScreen(),
     );
   }
 }
-
